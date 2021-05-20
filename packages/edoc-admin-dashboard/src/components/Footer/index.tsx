8df700df10329a185e4a581527f2efeb532e8730
EuphoriaCHN <879969355@qq.com>
2021-05-20 14:58:51 +0800

    style: 💄 Footer Styles

--- packages/edoc-admin-dashboard/src/components/Footer/index.tsx
+++ packages/edoc-admin-dashboard/src/components/Footer/index.tsx
@@ -10,22 +10,22 @@ interface IProps {
 }
 
 function Footer(props: IProps) {
-    const startYear = React.useMemo<number>(() => 2021, []);
-    const nowYear = new Date().getFullYear();
+  const startYear = React.useMemo<number>(() => 2021, []);
+  const nowYear = new Date().getFullYear();
 
-    return (
-        <Layout.Footer>
-            <div>
-                <Typography.Text>海弘建站</Typography.Text>
-                <Typography.Text>&copy; {nowYear === startYear ? startYear : `${startYear} ~ ${nowYear}`}</Typography.Text>
-                <Typography.Text>Xi`an University of Science and Technology</Typography.Text>
-            </div>
-            <div className={'site-footer-beian'}>
-                <img src={BeiAnLogo} />
-                <a target={'__blank'} href={'http://www.beian.miit.gov.cn/'}>陕ICP备 2021006075号-1</a>
-            </div>
-        </Layout.Footer>
-    );
+  return (
+    <Layout.Footer className={'site-footer'}>
+      <div>
+        <Typography.Text>海弘建站</Typography.Text>
+        <Typography.Text>&copy; {nowYear === startYear ? startYear : `${startYear} ~ ${nowYear}`}</Typography.Text>
+        <Typography.Text>Xi`an University of Science and Technology</Typography.Text>
+      </div>
+      <div className={'site-footer-beian'}>
+        <img src={BeiAnLogo} />
+        <a target={'__blank'} href={'http://www.beian.miit.gov.cn/'}>陕ICP备 2021006075号-1</a>
+      </div>
+    </Layout.Footer>
+  );
 }
 
 export default Footer;
